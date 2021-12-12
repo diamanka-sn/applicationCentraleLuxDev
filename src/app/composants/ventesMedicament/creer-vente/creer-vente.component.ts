3f7ed1a40b63924dfeda6818636bbc354fdaf0a4
alassDiallo <alassdiallo58@gmail.com>
2021-12-12 22:28:50 +0000

    vente-medoc

--- src/app/composants/ventesMedicament/creer-vente/creer-vente.component.ts
+++ src/app/composants/ventesMedicament/creer-vente/creer-vente.component.ts
@@ -12,26 +12,26 @@ import { ServiceventeService } from 'src/app/services/servicevente.service';
 })
 export class CreerVenteComponent implements OnInit {
 
-  formGroup!: FormGroup ;
-  subVente!: Subscription ;
-  
+  formGroup!: FormGroup;
+  subVente!: Subscription;
+
   venteMedocs: any[] = [];
 
-  medicaments!: any[] ;
-  subMedicament!: Subscription ;
+  medicaments!: any[];
+  subMedicament!: Subscription;
 
   dtOptions: DataTables.Settings = {}
 
-  libelle!: string ;
+  libelle!: string;
   coefficient!: number;
-  prixSession!: number ;
-  total: number = 0 ;
-  
+  prixSession!: number;
+  total: number = 0;
 
-  constructor(private form: FormBuilder, 
-              private router: Router, 
-              private servicevente: ServiceventeService,
-              private servicemedicament: ServicemedicamentService) { }
+
+  constructor(private form: FormBuilder,
+    private router: Router,
+    private servicevente: ServiceventeService,
+    private servicemedicament: ServicemedicamentService) { }
 
   ngOnInit(): void {
 
@@ -47,18 +47,18 @@ export class CreerVenteComponent implements OnInit {
 
     }
 
-    this.getAllMedicament() ;
-    this.initForm() ;
-    this.servicevente.subvente.subscribe() ;
+    this.getAllMedicament();
+    this.initForm();
+    this.servicevente.subvente.subscribe();
   }
 
   getAllMedicament() {
     this.subMedicament = this.servicemedicament.medocsubject.subscribe(
       (medocs: any[]) => {
-        this.medicaments = medocs ;
+        this.medicaments = medocs;
       }
     );
-    this.servicemedicament.getMedicament() ;
+    this.servicemedicament.getMedicament();
   }
 
   initForm() {
@@ -69,12 +69,12 @@ export class CreerVenteComponent implements OnInit {
   }
 
   selected(m: any) {
-    this.libelle = m.libelle ;
-    this.prixSession = m.prixSession ;
-    this.coefficient = m.coefficient ;
-    console.log('******************************       ' + this.libelle) ;
-    console.log('******************************       ' + this.prixSession) ;
-    console.log('******************************       ' + this.coefficient) ;
+    this.libelle = m.libelle;
+    this.prixSession = m.prixSession;
+    this.coefficient = m.coefficient;
+    console.log('******************************       ' + this.libelle);
+    console.log('******************************       ' + this.prixSession);
+    console.log('******************************       ' + this.coefficient);
 
   }
 
@@ -83,48 +83,52 @@ export class CreerVenteComponent implements OnInit {
     console.log(this.libelle);
     const quantite = this.formGroup.value['quantite'];
     console.log(quantite)
-    this.total = this.total + (this.prixSession * this.coefficient * quantite) ;
+    this.total = this.total + (this.prixSession * this.coefficient * quantite);
 
     // const m = [this.libelle, quantite]
     const isInTab = this.venteMedocs.filter((v) => {
-      return v.libelle == this.libelle ;
+      return v.libelle != this.libelle;
     });
-    if(isInTab.length != 0){ 
-      console.log( "********isInTab   " + isInTab[0].libelle ) ;
+    if (isInTab.length != 0) {
+      console.log("********isInTab   " + isInTab[0].libelle);
       // this.venteMedocs.
     }
-    
-    const m = {libelle:this.libelle, quantite: quantite, coefficient:this.coefficient, prixSession:this.prixSession} ;
+
+    const m = { libelle: this.libelle, quantite: quantite, coefficient: this.coefficient, prixSession: this.prixSession };
     this.venteMedocs.push(m);
 
 
-    this.formGroup.reset() ;
-    this.router.navigate(['espace/creer-vente']) ;
+    this.formGroup.reset();
+    this.medicaments = this.medicaments.filter(medicament => {
+      return medicament.libelle != m.libelle
+    })
+    console.log(this.medicaments)
+    // this.router.navigate(['espace/creer-vente']);
   }
 
   submit() {
-    const libelle = this.formGroup.value['libelle'] ;
-    const quantite = this.formGroup.value['quantite'] ;
-    console.log('******  ' + libelle) ;
-    console.log('*****    ' + quantite) ;
+    const libelle = this.formGroup.value['libelle'];
+    const quantite = this.formGroup.value['quantite'];
+    console.log('******  ' + libelle);
+    console.log('*****    ' + quantite);
     const vente = {
       libelle: libelle,
       quantite: quantite
     };
-    
-    this.servicevente.addVente(vente) ;
-    this.formGroup.reset() ;
-    this.router.navigate(['/espace/creer-vente']) ;
-    
+
+    this.servicevente.addVente(vente);
+    this.formGroup.reset();
+    // this.router.navigate(['/espace/creer-vente']) ;
+
   }
 
   addMedoc() {
-    const libelle = this.formGroup.value['libelle'] ;
-    console.log("$$$$$$       " + libelle) ;
-    const quantite = this.formGroup.value['quantite'] ;
-    console.log("$$$$$$$    " + quantite) ;
+    const libelle = this.formGroup.value['libelle'];
+    console.log("$$$$$$       " + libelle);
+    const quantite = this.formGroup.value['quantite'];
+    console.log("$$$$$$$    " + quantite);
   }
 
-  
+
 
 }
