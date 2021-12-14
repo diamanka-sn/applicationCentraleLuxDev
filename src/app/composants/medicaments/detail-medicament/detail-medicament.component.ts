69a53d1e47720ade505bee627950cb515c793cc4
diamanka98 <diamanka1998@gmail.com>
2021-12-14 18:35:35 +0000

    Details medicament fin

--- src/app/composants/medicaments/detail-medicament/detail-medicament.component.ts
+++ src/app/composants/medicaments/detail-medicament/detail-medicament.component.ts
@@ -25,8 +25,17 @@ export class DetailMedicamentComponent implements OnInit {
 
   
   type1 = 'doughnut';
-  data1 = {
-    labels: ["Medicament", "Autres medicaments"],
+  dataVentes = {
+    labels: ["Ventes", "Autres"],
+    datasets: [
+      {
+        data: [25, 50],
+        backgroundColor: ['#ff0266', '#81c784']
+      }
+    ]
+  };
+  dataDepenses = {
+    labels: ["Depenses", "Autres"],
     datasets: [
       {
         data: [25, 50],
@@ -42,7 +51,40 @@ export class DetailMedicamentComponent implements OnInit {
     },
     responsive: true,
     maintainAspectRatio: false,
-
   }
 
+  chartColors = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(201, 203, 207)'
+  };
+
+  mutiLineChartData = {
+    labels: ['Jan', 'Fev', 'Mars', 'Avr', 'Mai', 'Juin', 'Jui','Sept', 'Oct', 'Nov','Dec'],
+    datasets: [{
+      label: 'Recettes',
+      borderColor: this.chartColors.green,
+      backgroundColor: this.chartColors.green,
+      data: [10, 30, 46, 2, 8, 50, 0],
+      fill: false,
+    }, {
+      label: 'Pertes',
+      borderColor: this.chartColors.red,
+      backgroundColor: this.chartColors.red,
+      data: [7, 49, 46, 13, 25, 30, 22],
+      fill: false,
+    }]
+  }
+  //position = "bottom"
+  mutiLineChartDataoptions = {
+    responsive: true,
+    title: {
+      display: true,
+     // text: '' + this.position
+    }
+  }
 }
