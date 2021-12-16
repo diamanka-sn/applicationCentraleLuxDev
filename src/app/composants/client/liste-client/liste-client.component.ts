855b6e44ac88e299745747a1c02c92fea049167a
alassDiallo <alassdiallo58@gmail.com>
2021-12-16 00:02:07 +0000

    ajout dernier

--- src/app/composants/client/liste-client/liste-client.component.ts
+++ src/app/composants/client/liste-client/liste-client.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { ServiceclientService } from 'src/app/services/serviceclient.service';
@@ -9,14 +10,14 @@ import { ServiceclientService } from 'src/app/services/serviceclient.service';
   styleUrls: ['./liste-client.component.css']
 })
 export class ListeClientComponent implements OnInit {
-
+  formGroup!: FormGroup;
   clients!: any[]
   dtOptions: DataTables.Settings = {};
   subsclient!: Subscription
-  constructor(private routes: Router, private serviceclient: ServiceclientService) { }
+  constructor(private routes: Router, private serviceclient: ServiceclientService, private form: FormBuilder) { }
 
   ngOnInit(): void {
-    this.getclient()
+
     this.dtOptions = {
       pagingType: 'numbers',
       pageLength: 5,
@@ -28,6 +29,57 @@ export class ListeClientComponent implements OnInit {
       // columns: [{ data: 'nom' }, { data: 'adresse' }, { data: 'ville' }, { data: 'age' }]
 
     }
+    this.initForm()
+    this.getclient()
+  }
+
+  initForm() {
+    this.formGroup = this.form.group({
+      nom: ['', [Validators.required]],
+      prenom: ['', [Validators.required]],
+      telephone: ['', [Validators.required]],
+      adresse: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      // motDePass: ['', [Validators.required]],
+    })
+  }
+
+  submit() {
+    const nom = this.formGroup.value['nom']
+    const prenom = this.formGroup.value['prenom']
+    const adresse = this.formGroup.value['adresse']
+    const telephone = this.formGroup.value['telephone']
+    const email = this.formGroup.value['email']
+    // const motDePass = this.formGroup.value['motDePass']
+
+    const client = {
+      nom: nom,
+      prenom: prenom,
+      adresse: adresse,
+      telephone: telephone,
+      email: email,
+      // motDePass: motDePass
+
+    }
+    this.serviceclient.addclient(client)
+    this.formGroup.reset()
+    $('#exampleModal').modal('hide')
+
+  }
+
+  modifier(client: any) {
+    this.formGroup.patchValue({
+      nom: client.nom,
+      prenom: client.prenom,
+      email: client.email,
+      telephone: client.telephone,
+      adresse: client.adresse
+    });
+    $('#exampleModal').modal('show')
+  }
+
+  supprimer(client: any) {
+    return confirm("Voulez-vous vraiment ssupprimé ce client")
   }
 
   getclient() {
--- src/app/composants/collaborations/liste-collaboration/liste-collaboration.component.ts
+++ src/app/composants/collaborations/liste-collaboration/liste-collaboration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { ServiceCollaborationService } from 'src/app/services/service-collaboration.service';
@@ -12,11 +13,12 @@ export class ListeCollaborationComponent implements OnInit {
 
   dtOptions: DataTables.Settings = {};
   subEntreprise!: Subscription;
+  formGroup!: FormGroup
   entreprises!: any[];
 
 
   constructor(private serviceCollaboration: ServiceCollaborationService,
-    private router: Router) { }
+    private router: Router, private entrepriseFormGroup: FormBuilder) { }
 
   ngOnInit(): void {
     this.dtOptions = {
@@ -29,9 +31,35 @@ export class ListeCollaborationComponent implements OnInit {
         "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 pull-right col-md-5' p>>",
       language: { url: 'assets/datatable-French.json' },
     }
+    this.initForm()
     this.getAllCollaboration();
   }
 
+  initForm() {
+    this.formGroup = this.entrepriseFormGroup.group({
+      nom: ['', [Validators.required, Validators.maxLength(100)]],
+      adresse: ['', [Validators.required, Validators.maxLength(100)]],
+      telephone: ['', [Validators.required, Validators.min(9), Validators.max(9)]],
+      email: ['', [Validators.required, Validators.email, Validators.maxLength(100)]],
+      tauxPEC: ['', [Validators.required]]
+    })
+  }
+
+  submit() {
+    alert('valider')
+  }
+
+  modifier(e: any) {
+    this.formGroup.patchValue({
+      nom: e.nom,
+      adresse: e.adresse,
+      telephone: e.telephone,
+      email: e.email,
+      tauxPEC: e.tauxPEC
+    });
+
+    $('#exampleModal').modal('show')
+  }
   getAllCollaboration() {
     this.subEntreprise = this.serviceCollaboration.subCollaboration.subscribe(
       (allEntreprises: any) => {
