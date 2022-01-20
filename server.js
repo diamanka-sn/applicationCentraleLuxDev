81e3704172d93726317a76e84d5d91e825502aaf
alassDiallo <alassdiallo58@gmail.com>
2022-01-20 13:10:48 +0000

    config heroku

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// app.use(express.static(__dirname + '/dist/application-central'));
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname +
+//         '/dist/application-central/index.html'));
+// });
+// app.listen(process.env.PORT || 8080);
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/application-central'));
+// const app = express();
+app.use(requireHTTPS);
+app.use(express.static('./dist/application-central'));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname +
-        '/dist/application-central/index.html'));
+    res.sendFile('index.html', { root: 'dist/application-central/' }
+    );
 });
 app.listen(process.env.PORT || 8080);
